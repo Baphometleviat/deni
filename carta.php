6334fb30c3fd05b3230e72590c6dce301dedf647
Baphometleviat <baphometleviat@gmail.com>
2016-09-09 00:17:16 -0500

    Terminado
    Pagina terminado

--- carta.php
+++ carta.php
@@ -1,26 +1,99 @@
 <html>
     <head>
-        <title>Escribe un nombre 7n7</title>
+        <title>09/03/16</title>
         <link rel="stylesheet" href="styles_carta.css" />
+        <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/rebecca" type="text/css"/> 
         <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
         <script src="scripts.js"></script>
     </head>
     <body >
         <div class="contenedor_primero">
+            <br>
             <h1>
                 Buenos días señorita Deni
             </h1>
-            <br>
+            <img src="corazon.png" id="corazon" class="emojisgrandes">
             <p class="intro">
                 <i>He dejado mi puerta entreabierta y entraste tú sin avisar... no te apartes de mi oh no... oh no</i>
             </p>
+            <br>            
+        </div>
+        <div class="contenedor_foto1"></div>
+        <div class="contenedor_segundo">
             <br>
+            <p class="carta">Si estás leyendo esta carta es que mi servidor sí sirvió jajaja y pues la verdad no sé cómo empezar, estoy bastante nervioso...</p>
+            <img src="nervioso.png" id="nervioso" class="emojisgrandes"><br>            
+            <p class="carta">
+            porque sabes, 6 meses se dice fácil, los minutos, las horas, los días, semanas, meses pasan sin más, así insipidos...
+            </p>
+            <img src="calendario.png" id="calendario" class="emojisgrandes">
+            <img src="reloj_arena.png" id="reloj_arena" class="emojisgrandes">
+            <br>
+        </div>
+        <div class="contenedor_foto2"></div>
+        <div class="contenedor_tercero">
+            <br>
+            <p class="carta">
+            pero tengo que confesar que desde aquél 19 de febrero no pasa un instante sin que sonría por tí, no pasa un momento que no estés en mi mente, dia y noche, hora tras hora eres todo lo que ocupa mi pensamiento...     
+            </p>
+            <img src="pensamiento.png" id="calendario" class="emojisgrandes">
+            <p class="carta">
+            no olvido la primera vez que te ví, eras la más hermosa y sin dudarlo te pedí que bailaras, quizá no fue el mejor lugar o el más romantico pero las mejores cosas suceden en los momentos más inesperados y tú... bueno, tú eres lo mejor que me ha pasado...
+            </p>
+            <br>
+            <img src="ojos_cora.png" id="ojos_cora" class="emojisgrandes">
+        </div>
+        <div class="contenedor_foto3"></div>
+        <div class="contenedor_cuarto">
+            <br>
+            <p class="carta">
+            cada día me levanto pensando en tí, en las palabras lindas que me dices, tus caricas, las canciones que nos hemos dedicado, pensando en el futuro que nos espera, recordando esos bellos momentos, nuestra primera cita, nuestor primer beso, cuando fuimos a la friki jajaja el primer mes es el más romántico pero los demás, han sido simplemente perfectos...
+            </p>
+            <img src="sushi.png" id="sushi" class="emojisgrandes">
+            <img src="ramen.png" id="ramen" class="emojisgrandes">
+            <br>
+            <p class="carta">
+            no puedo dejar de darte las gracias por amarme, por haberme hecho creer en el amor de nuevo, por haberme dado una nueva razón, gracias porque contigo me siento felíz y sobre todo, gracias por aguantarme...
+            </p>
+            <img src="sonrojado.png" id="sonrojado" class="emojisgrandes">
+            <img src="sonrisa.png" id="sonrisa" class="emojisgrandes">
+            <br>
+            <p class="carta">
+            por aguantar mis celos, mis enojos sin razón, mis corajes, mis berrinches de niño...
+            </p>
+            <img src="triste.png" id="triste" class="emojisgrandes">
+            <img src="bah.png" id="bah" class="emojisgrandes">
+            <p class="carta">
+            pero sinceramente, me has vuelto un niño, dependiente de tí, conoces mis más profundos miedos, temores y debilidades, tengo a miedo a perderte te necesito para vivir, sin tí la vida es gris, no soporto esos días donde tengo que trabajar o ir a la escuela hasta las 7, me matan los días que paso sin verte, esas noches deseando tenerte a mi lado se me hacen eternas, me calan hasta los huesos, hasta las lágrimas...</p>
+            <img src="dolor.png" id="dolor" class="emojisgrandes">
+        </div>
+        <div class="contenedor_foto4"></div>
+        <div class="contenedor_quinto">
+        <br>
+            <p class="carta">
+            nadie dijo que sería fácil, hemos tenido que soportar peleas y gritos, malos entendidos, lágrimas y celos pero debemos ser como el cactus, tenemos que estar siempre arriba del barco...
+            </p>
             <img src="barco.png" id="barco" class="emojisgrandes">
             <img src="cactus.png" id="cactus" class="emojisgrandes">
+            <br>
+            <p class="carta">
+            para que al voltear nos demos cuenta que ha valido la pena, que todo ese esfuerzo no fue en vano, cuando hayamos recorrido el mundo, cuando estemos en Canadá, cuando nos vayamos al bosque con Cory y Nery, cuando tengamos la casa bonita, cuando llegue el momento en el que no te diga "adiós" sino "hasta mañana" porque llegará el momento que nuestros cepillos de dientes estén en el mismo vasito...
+            </p>
+            <br>
+            <img src="changuito-boca.png" id="changuito" class="emojisgrandes">            
+            <img src="manoizq.png" id="nieve" class="emojisgrandes">
+            <img src="manoder.png" id="nieve" class="emojisgrandes">
+            <br>
+            <p class="carta">
+            Felices 6 meses, gracias por todo lo que me has dado, y aunque todo el mundo lo sepa, lo repita y repita, más que nadie debes saber que eres el amor de mi vida, de mis sueños, simplemente eres mi vida, simplemente...
+            </p>
         </div>
-        <div class="contenedor_vacio"></div>
-        <div class="contenedor_segundo">
-            
-        </div>
+        <div class="barco">
+            <br>
+            <br>
+            <p class="final">Te Amo</p>
+            <br>
+            <p class="carta"><i>y te amo como nadie te ha querido, te amo como nadie te amará no creas que son sólo palabritas...</i></p>
+        </div>                            
     </body>
 </html>
